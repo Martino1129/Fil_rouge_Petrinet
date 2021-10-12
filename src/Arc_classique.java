d7bbb0ca7b0624db13457a2a640cb9dcd43ce92d
TitouenDCL <91615736+TitouenDCL@users.noreply.github.com>
2021-10-12 22:45:16 +0200

    update
    montrer a louis

--- src/Arc_classique.java
+++ src/Arc_classique.java
@@ -11,7 +11,7 @@ public class Arc_classique implements IArc{
 	public boolean arc_valide() {
 		if (this.nb_jetons <= this.place_a.get_nb_jetons()) {
 			return true; 
-			//test 
+			//test gros pd
 		}
 		return false;
 	}
