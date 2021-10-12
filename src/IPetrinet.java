6e937bde1b4856c46f554929ee37345f9a6bffc1
TitouenDCL <91615736+TitouenDCL@users.noreply.github.com>
2021-10-12 14:21:12 +0200

    Update IPetrinet.java

--- src/IPetrinet.java
+++ src/IPetrinet.java
@@ -1,4 +1,12 @@
-
 public interface IPetrinet {
-
+	
+	public void createPlace(int n);
+	public void createTransitition();
+	public void createArc();
+	public void triggerTransition();
+	public void removePlace();
+	public void removeArc();
+	public void removeTransition();
+	
+	
 }
