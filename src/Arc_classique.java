78c8eddd28c3b9b64dd29b13c61b327d23689c42
Martino1129 <martin.ducros@imt-atlantique.net>
2021-10-12 09:44:28 +0200

    Update Arc_classique.java

--- src/Arc_classique.java
+++ src/Arc_classique.java
@@ -11,6 +11,7 @@ public class Arc_classique extends Arc{
 	public boolean arc_valide() {
 		if (this.nb_jetons <= this.place_a.get_nb_jetons()) {
 			return true; 
+			//test 
 		}
 		return false;
 	}
