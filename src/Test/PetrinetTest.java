4d10bbd4c062bfa3d327d2bb53d8b6bcc325f09b
TitouenDCL <91615736+TitouenDCL@users.noreply.github.com>
2021-10-19 08:06:30 +0200

    Tous les test Petrinet sauf les remove

--- src/Test/PetrinetTest.java
+++ src/Test/PetrinetTest.java
@@ -3,9 +3,11 @@ package Test;
 import java.util.LinkedList;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 
 import Petri.Arc_classique;
@@ -18,7 +20,10 @@ import Petri.Place;
 import Petri.Transition;
 import Petri.TypeException;
 
+@TestMethodOrder(OrderAnnotation.class)
+
 public class PetrinetTest {
+	
 	static Petrinet petrinet = null;
 	static Place P1 = null;
 	static Place P2 = null;
@@ -37,7 +42,7 @@ public class PetrinetTest {
 	@BeforeAll
 	static void setup() throws TypeException, ImpossibleAction {
 		
-		System.out.println("BeforeAll !!");
+		System.out.println("Implementation de petrinet");
 		/**
 		 * Firstly we create a pretri diagram, then 2 places , 3 Transitions and 7 arcs
 		 */
@@ -77,8 +82,7 @@ public class PetrinetTest {
 		System.out.println();
 
 	}
-
-
+	@Order(1)
 	@Test
 	public void testPlaces() throws TypeException, ImpossibleAction {
 
@@ -87,26 +91,97 @@ public class PetrinetTest {
 		Assertions.assertEquals(petrinet.getPlaces().get(0), P1);
 		Assertions.assertEquals(petrinet.getPlaces().get(1), P2);
 		
-		System.out.println("Places bien instaures dans petrinet");
+		System.out.println("Places: OK");
 	}
 	
+	@Order(2)
 	@Test
-	public void testTransition() throws TypeException, ImpossibleAction {
+	public void testTransitions() throws TypeException, ImpossibleAction {
 
 		Assertions.assertEquals(petrinet.getTransitions().size(),3);
 		
 		Assertions.assertEquals(petrinet.getTransitions().get(0), T1);
 		Assertions.assertEquals(petrinet.getTransitions().get(1), T2);
-		System.out.println("Transitions bien instaures dans petrinet");
+		System.out.println("Transitions: OK");
 	}
 	
+	@Order(3)
 	@Test
-	public void testArc() throws TypeException, ImpossibleAction {
+	public void testArcs() throws TypeException, ImpossibleAction {
 
 		Assertions.assertEquals(petrinet.getArcs().size(),7);
-		Assertions.assertEquals(petrinet.getTransitions().get(2), T2P1);
-		Assertions.assertEquals(petrinet.getTransitions().get(6), P2T3);
-		Assertions.assertEquals(expected, actual);
-		System.out.println("Transitions bien instaures dans petrinet");
+		Assertions.assertEquals(petrinet.getArcs().get(1), T2P1);
+		Assertions.assertEquals(petrinet.getArcs().get(5), P2T3);
+		Assertions.assertTrue(petrinet.getArcs().contains(T1P1));
+		Assertions.assertTrue(petrinet.getArcs().contains(P1T3));
+
+		
+		Assertions.assertEquals(T1.getArc_e().size(),1);
+		Assertions.assertTrue(T1.getArc_e().contains(P2T1));
+		Assertions.assertEquals(T1.getArc_s().size(),1);
+		Assertions.assertTrue(T1.getArc_s().contains(T1P1));
+		
+		Assertions.assertEquals(T3.getArc_e().size(),2);
+		Assertions.assertEquals(T3.getArc_s().size(),1);
+		Assertions.assertTrue(T3.getArc_e().contains(P2T3));
+		Assertions.assertTrue(T3.getArc_e().contains(P1T3));
+		Assertions.assertTrue(T3.getArc_s().contains(T3P2));
+		
+
+		System.out.println("Arcs: OK");
+
+	}
+	
+	@Order(4)
+	@Test
+	public void testT1() throws TypeException, ImpossibleAction {
+		petrinet.triggerTransition(T1);
+		System.out.println("T1 Triggered, checking if petrinet is not damaged");
+		testPlaces();
+		testTransitions();
+		testArcs();
+	}
+	@Order(5)
+	@Test
+	public void testT1x5() throws TypeException, ImpossibleAction {
+		petrinet.triggerTransition(T1);
+		petrinet.triggerTransition(T1);
+		petrinet.triggerTransition(T1);
+		petrinet.triggerTransition(T1);
+		petrinet.triggerTransition(T1);
+
+		System.out.println("T1 Triggered, checking if petrinet is not damaged");
+		testPlaces();
+		testTransitions();
+		testArcs();
+	}
+	@Order(6)
+	@Test
+	public void testT2() throws TypeException, ImpossibleAction {
+		petrinet.triggerTransition(T2);
+		System.out.println("T2 Triggered, checking if petrinet is not damaged");
+		testPlaces();
+		testTransitions();
+		testArcs();
+	}
+	
+	@Order(7)
+	@Test
+	public void testT3() throws TypeException, ImpossibleAction {
+		petrinet.triggerTransition(T3);
+		System.out.println("T3 Triggered, checking if petrinet is not damaged");
+		testPlaces();
+		testTransitions();
+		testArcs();
+	}
+	
+	@Order(7)
+	@Test
+	public void testremoveArc() throws TypeException, ImpossibleAction {
+		petrinet.removeArc(P2T3);
+/*		Assertions.assertTrue(T3.getArc_e().contains(P2T3)); */
+		Assertions.assertEquals(petrinet.getArcs().size(), 6);
+		System.out.println("");
+
 	}
 }
